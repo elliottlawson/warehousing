b871a87a57b4212b363b81298d54958337d25731
Elliott Lawson <elliott.lawson@outlook.com>
2021-10-06 22:09:08 -0400

    Update PHP-CS-Fixer Rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,31 +11,37 @@ $finder = Symfony\Component\Finder\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR12'                            => true,
-        'array_syntax'                      => ['syntax' => 'short'],
-        'ordered_imports'                   => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports'                 => true,
-        'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline'       => true,
-        'phpdoc_scalar'                     => true,
-        'unary_operator_spaces'             => true,
-        'binary_operator_spaces'            => true,
-        'align_double_arrow'                => true,
-        'align_equals'                      => true,
-        'blank_line_before_statement'       => [
-            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+        '@PSR12'                                => true,
+        'array_syntax'                          => ['syntax' => 'short'],
+        'ordered_imports'                       => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports'                     => true,
+        'lowercase_cast'                        => true,
+        'not_operator_with_successor_space'     => true,
+        'trim_array_spaces'                     => true,
+        'trailing_comma_in_multiline'           => true,
+        'no_trailing_comma_in_singleline_array' => true,
+        'phpdoc_scalar'                         => true,
+        'unary_operator_spaces'                 => true,
+        'binary_operator_spaces'                => [
+            'operators' => [
+                '='  => 'align_single_space_minimal',
+                '=>' => 'align_single_space_minimal',
+            ],
         ],
-        'phpdoc_single_line_var_spacing'    => true,
-        'phpdoc_var_without_name'           => true,
-        'class_attributes_separation'       => [
+        'blank_line_before_statement'           => [
+            'statements' => ['break', 'continue', 'declare', 'return ', 'throw', 'try'],
+        ],
+        'phpdoc_single_line_var_spacing'        => true,
+        'phpdoc_var_without_name'               => true,
+        'class_attributes_separation'           => [
             'elements' => [
                 'method' => 'one',
             ],
         ],
-        'method_argument_space'             => [
+        'method_argument_space'                 => [
             'on_multiline'                     => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
-        'single_trait_insert_per_statement' => true,
+        'single_trait_insert_per_statement'     => true,
     ])
     ->setFinder($finder);
